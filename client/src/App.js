c4406f8b53b0d305b6724a6c597235bb812fcbb1
Jonah Eagleson <j-money@Jonahs-MacBook-Air.local>
2025-03-11 15:27:26 -0400

    Updated landing page

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,11 @@ function App() {
     <div className="app-container">
       <h1>wluNest</h1>
       <p>Discover and compare apartment buildings, floor plans, and amenities in your area.</p>
-      <button>Get Started</button>
+      <div className='button-group'>
+        <button className='browse-btn'>Browse</button>
+        <span className='or-text'>or</span>
+        <button className='login-btn'>Login</button>
+      </div>
     </div>
   );
 }
