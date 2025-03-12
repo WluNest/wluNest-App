3a1b64d0325a804e76cbbd0f05ecd38cbaf1cb21
aaronkim15 <aaronkim135@gmail.com>
2025-03-12 18:25:06 -0400

    changes to the table attributes

--- server/schema.sql
+++ server/schema.sql
@@ -2,19 +2,18 @@ CREATE DATABASE wluNest;
 USE wluNest;
 
 # Creates the users table
-CREATE TABLE users{
+CREATE TABLE users(
     user_id SERIAL PRIMARY KEY UNIQUE auto_increment,
     username VARCHAR(255) UNIQUE NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     role ENUM('admin', 'user') DEFAULT 'user'
-
-};
+);
 #Creates the listings table
-CREATE TABLE listings{
+CREATE TABLE listings(
     listing_id SERIAL PRIMARY KEY UNIQUE auto_increment,
     user_id INT NOT NULL,
     title VARCHAR(255) NOT NULL,
@@ -26,10 +25,10 @@ CREATE TABLE listings{
     bed INT NOT NULL,
     bath INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
-};
+);
 
 #Creates the review table
-CREATE TABLE review{
+CREATE TABLE review(
     review_id SERIAL PRIMARY KEY UNIQUE auto_increment,
     user_id INT NOT NULL,
     listing_id INT NOT NULL,
@@ -38,77 +37,83 @@ CREATE TABLE review{
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (listing_id) REFERENCES listings(listing_id)
-}
+);
 
 #Creates the property table
-CREATE TABLE property {
-    property_id SERIAL PRIMARY KEY UNIQUE auto_increment,
-    listing_id INT NOT NULL,
-    unit_number VARCHAR(255) NULL,
-    street_name VARCHAR(255) NOT NULL,
+CREATE TABLE property
+(
+    property_id   SERIAL PRIMARY KEY UNIQUE auto_increment,
+    listing_id    INT          NOT NULL,
+    unit_number   VARCHAR(255) NULL,
+    street_name   VARCHAR(255) NOT NULL,
     street_number VARCHAR(255) NOT NULL,
-    city VARCHAR(255) NOT NULL,
-    province VARCHAR(255) NOT NULL,
-    postal_code VARCHAR(255) NOT NULL,
-    FOREIGN KEY (listing_id) REFERENCES listings(listing_id)
-}
+    city          VARCHAR(255) NOT NULL,
+    province      VARCHAR(255) NOT NULL,
+    postal_code   VARCHAR(255) NOT NULL,
+    FOREIGN KEY (listing_id) REFERENCES listings (listing_id)
+);
 
 #Creates the floor_plan table
-CREATE TABLE floor_plan {
-    floor_plan_id SERIAL PRIMARY KEY UNIQUE auto_increment,
-    property_id INT NOT NULL,
-    floor_plan_name VARCHAR(255) NOT NULL,
+CREATE TABLE floor_plan
+(
+    floor_plan_id    SERIAL PRIMARY KEY UNIQUE auto_increment,
+    property_id      INT          NOT NULL,
+    floor_plan_name  VARCHAR(255) NOT NULL,
     floor_plan_image VARCHAR(255) NOT NULL,
-    FOREIGN KEY (property_id) REFERENCES property(property_id)
-}
+    FOREIGN KEY (property_id) REFERENCES property (property_id)
+);
 
 #Creates the amentites
-CREATE TABLE amentites {
+CREATE TABLE amentites (
     amentites_id SERIAL PRIMARY KEY UNIQUE auto_increment,
     property_id INT NOT NULL,
     amentites_name VARCHAR(255) NOT NULL,
     FOREIGN KEY (property_id) REFERENCES property(property_id)
-}
+    );
 
 
 #juntions tables 
 
 #Connects the property and amentites table to save the amentites to a specific property
-CREATE TABLE property_amenities {
-    property_amenities_id SERIAL PRIMARY KEY UNIQUE,
+CREATE TABLE property_amenities
+(
+    property_amenities_id SERIAL UNIQUE,
     property_id INT NOT NULL,
-    amentites_id INT NOT NULL,
-    FOREIGN KEY (property_id) REFERENCES property(property_id),
-    FOREIGN KEY (amentites_id) REFERENCES amentites(amentites_id)
-}
+    amentites_id    INT NOT NULL,
+    FOREIGN KEY (property_id) REFERENCES property (property_id) ON DELETE CASCADE,
+    FOREIGN KEY (amentites_id) REFERENCES amentites (amentites_id) ON DELETE CASCADE
+);
 
 #Connects the user and listing table to save users favorite listings
-CREATE TABLE user_saves {
-    user_saves_id SERIAL PRIMARY KEY UNIQUE,
+CREATE TABLE user_saves
+(
+    user_saves_id SERIAL UNIQUE,
     user_id INT NOT NULL,
-    listing_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (listing_id) REFERENCES listings(listing_id)
-}
+    listing_id  INT NOT NULL,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
+    FOREIGN KEY (listing_id) REFERENCES listings (listing_id) ON DELETE CASCADE
+);
 
 #Connects the user and review table to show users reviews
-CREATE TABLE user_reviews {
-    user_reviews_id SERIAL PRIMARY KEY UNIQUE,
+CREATE TABLE user_reviews
+(
+    user_reviews_id SERIAL UNIQUE,
     user_id INT NOT NULL,
-    review_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (review_id) REFERENCES review(review_id)
-}
+    review_id   INT NOT NULL,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
+    FOREIGN KEY (review_id) REFERENCES review (review_id) ON DELETE CASCADE
+);
 
 #Connects the user and listing tables to show users listings
-CREATE TABLE user_listings {
-    user_listings_id SERIAL PRIMARY KEY UNIQUE auto_increment,
+CREATE TABLE user_listings
+(
+    user_listings_id SERIAL UNIQUE,
     user_id INT NOT NULL,
-    listing_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (listing_id) REFERENCES listings(listing_id)
-}
+    listing_id  INT NOT NULL,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (listing_id) REFERENCES listings (listing_id)
+);
 
