8903dd6a6ec161ef0499a4e4e57f88a7e3623195
Jonah Eagleson <j-money@Jonahs-MacBook-Air.local>
2025-03-12 12:53:20 -0400

    Added NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import React from "react";
+import Navbar from './Navbar'
 
 function App() {
   return (
-    <div className="app-container">
-      <h1>wluNest</h1>
-      <p>Discover and compare apartment buildings and floor plans around WLU.</p>
-      <div className='button-group'>
-        <button className='browse-btn'>Browse</button>
-        <span className='or-text'>or</span>
-        <button className='login-btn'>Login</button>
+    <>
+      <Navbar />
+      <div className="app-container">
+        <h1>wluNest</h1>
+        <p>Discover and compare apartment buildings and floor plans around WLU.</p>
+        <div className='button-group'>
+          <button className='browse-btn'>Browse</button>
+          <span className='or-text'>or</span>
+          <button className='login-btn'>Login</button>
+        </div>
       </div>
-    </div>
+    </> 
   );
 }
 
---
+++ client/src/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <a href="/" className="logo">
+        wluNest
+      </a>
+    </nav>
+  );
+}
+
+export default Navbar;
