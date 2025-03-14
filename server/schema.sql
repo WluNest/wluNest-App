22908888b835f68ed266abde0b4532e53be3da61
aaronkim15 <aaronkim135@gmail.com>
2025-03-13 20:20:24 -0400

    changes to schema

--- server/schema.sql
+++ server/schema.sql
@@ -3,7 +3,7 @@ USE wluNest;
 
 # Creates the users table
 CREATE TABLE users(
-    user_id INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
+    users_id INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
     username VARCHAR(255) UNIQUE NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
@@ -15,29 +15,29 @@ CREATE TABLE users(
 #Creates the listings table
 CREATE TABLE listings(
     listing_id INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
-    user_id INT NOT NULL,
+    users_id INT NOT NULL,
     title VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL CHECK (price >= 0),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     created_by_admin BOOLEAN DEFAULT FALSE,
-    listing_image BLOB NOT NULL,
+    listing_image VARCHAR(255) NOT NULL,
     bed INT NOT NULL,
     bath INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    FOREIGN KEY (users_id) REFERENCES users(users_id) ON DELETE CASCADE
 );
 
 #Creates the review table
 CREATE TABLE review(
     review_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
+    users_id INT NOT NULL,
     listing_id INT NOT NULL,
-    rating INT NOT NULL,
+    rating INT NOT NULL CHECK (rating >= 0 AND rating <= 5),
     description TEXT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (listing_id) REFERENCES listings(listing_id),
-    UNIQUE(user_id, listing_id) #Ensures one user can only review a listing once.
+    FOREIGN KEY (users_id) REFERENCES users(users_id) ON DELETE CASCADE,
+    FOREIGN KEY (listing_id) REFERENCES listings(listing_id) ON DELETE CASCADE,
+    UNIQUE(users_id, listing_id) #Ensures one user can only review a listing once.
 );
 
 #Creates the property table
@@ -51,7 +51,7 @@ CREATE TABLE property
     city          VARCHAR(255) NOT NULL,
     province      VARCHAR(255) NOT NULL,
     postal_code   VARCHAR(255) NOT NULL,
-    FOREIGN KEY (listing_id) REFERENCES listings (listing_id)
+    FOREIGN KEY (listing_id) REFERENCES listings (listing_id) ON DELETE CASCADE
 );
 
 #Creates the floor_plan table
@@ -60,8 +60,8 @@ CREATE TABLE floor_plan
     floor_plan_id    INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
     property_id      INT          NOT NULL,
     floor_plan_name  VARCHAR(255) NOT NULL,
-    floor_plan_image BLOB NOT NULL,
-    FOREIGN KEY (property_id) REFERENCES property (property_id)
+    floor_plan_image VARCHAR(255) NOT NULL,
+    FOREIGN KEY (property_id) REFERENCES property (property_id) ON DELETE CASCADE
 );
 
 #Creates the amenities
@@ -69,7 +69,7 @@ CREATE TABLE amenities (
     amenities_id INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
     property_id INT NOT NULL,
     amenities_name VARCHAR(255) NOT NULL,
-    FOREIGN KEY (property_id) REFERENCES property(property_id)
+    FOREIGN KEY (property_id) REFERENCES property(property_id) ON DELETE CASCADE
     );
 
 #juntions tables 
@@ -87,37 +87,37 @@ CREATE TABLE property_amenities
 
 #Connects the user and listing table to save users favorite listings
 # (Users can save multiple listings)
-CREATE TABLE user_saves
+CREATE TABLE users_saves
 (
-    user_saves_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
+    users_saves_id INT AUTO_INCREMENT PRIMARY KEY,
+    users_id INT NOT NULL,
     listing_id  INT NOT NULL,
     created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
+    FOREIGN KEY (users_id) REFERENCES users (users_id) ON DELETE CASCADE,
     FOREIGN KEY (listing_id) REFERENCES listings (listing_id) ON DELETE CASCADE
 );
 
 #Connects the user and review table to show users reviews
 #(Users can create multiple reviews)
-CREATE TABLE user_reviews
+CREATE TABLE users_reviews
 (
-    user_reviews_id INT AUTO_INCREMENT UNIQUE PRIMARY KEY,
-    user_id INT NOT NULL,
+    users_reviews_id INT AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    users_id INT NOT NULL,
     review_id   INT NOT NULL,
     created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
+    FOREIGN KEY (users_id) REFERENCES users (users_id) ON DELETE CASCADE,
     FOREIGN KEY (review_id) REFERENCES review (review_id) ON DELETE CASCADE
 );
 
 #Connects the user and listing tables to show users listings(Users can create multiple listings)
-CREATE TABLE user_listings
+CREATE TABLE users_listings
 (
     user_listings_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
+    users_id INT NOT NULL,
     listing_id  INT NOT NULL,
     created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users (user_id),
-    FOREIGN KEY (listing_id) REFERENCES listings (listing_id)
+    FOREIGN KEY (users_id) REFERENCES users (users_id) ON DELETE CASCADE,
+    FOREIGN KEY (listing_id) REFERENCES listings (listing_id) ON DELETE CASCADE
 );
 
