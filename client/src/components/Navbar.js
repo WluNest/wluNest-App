b28713b5ef19f00010ccf000b74a270dfd3af771
Luka117 <kowanskilukas145@gmail.com>
2025-03-15 12:11:25 -0400

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./Navbar.css";
+import { useState } from "react";
 
-function Navbar({ setCurrentPage }) {
+function Navbar({ setCurrentPage, user }) {
+  const handleAccountClick = () => {
+    if (user) {
+      setCurrentPage("account");
+    } else {
+      setCurrentPage("login");
+    }
+  }
+  
   return (
     <nav className="navbar">
       <span className="logo" onClick={() => setCurrentPage("home")}>wluNest</span>
       <div className="nav-links">
         <span className="nav-item" onClick={() => setCurrentPage("listings")}>Browse Listings</span>
-        <span className="nav-item" onClick={() => setCurrentPage("account")}>Account</span>
+        <span className="nav-item" onClick={() => setCurrentPage(handleAccountClick)}>{ user ? "Account" : "Login"}</span>
       </div>
     </nav>
   );
