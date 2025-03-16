d061ad75e1860f075a7e2130df6dae69b9a6831b
Luka117 <kowanskilukas145@gmail.com>
2025-03-16 13:30:03 -0400

    Update Account.jsx

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-const Account = () => {
+const Account = ({ user }) => {
   return (
-    <div>Account</div>
-  )
-}
+    <div className="account-container">
+      {user ? (
+        <div className="account-details">
+          <h2>Welcome, {user.name}</h2>
+          <p>Email: {user.email}</p>
+          <div className="account-actions">
+            <button className="edit-btn">Edit Profile</button>
+            <button className="logout-btn">Logout</button>
+          </div>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
 
-export default Account
+export default Account;
