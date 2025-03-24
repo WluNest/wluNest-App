eae94c66b6ee2a456ad9c9813af302db8d5e3734
Luka118 <kowanskilukas145@gmail.com>
2025-03-24 16:53:29 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import ListingCreate from "./pages/ListingCreate";
 import Login from "./pages/Login";
 import Settings from "./pages/Settings";
 import Buildings from "./pages/Buildings";
+import Roommates from "./pages/Roommates";
 
 function App() {
   const [currentPage, setCurrentPage] = useState("home");
@@ -42,6 +43,8 @@ function App() {
         <Settings />
       ) : currentPage === "buildings" ? (
         <Buildings />
+      ) : currentPage === "roommates" ? (
+        <Roommates />
       ) : null}
     </div>
   );
