32edba50b5ca3a4a4bb8208f841235166becf260
Paarth Bagga <paarthbagga@icloud.com>
2025-03-27 00:52:55 -0400

    Update insert.sql
    Updated insert to work with updated schema, and added correlating dummy data

--- server/insert.sql
+++ server/insert.sql
@@ -7,37 +7,19 @@ VALUES
 ('user2', 'Arya', 'Singh', 'aryaspassword', 'arya@gmail.com', 'user'),
 ('user3', 'Aaron', 'Kim', 'aaronspass', 'aaron@wlunest.com', 'admin');
 
-INSERT INTO listings (users_id, unit_number, title, description, price, listing_image, bed, bath)
+INSERT INTO listings (users_id, unit_number, title, description, price, listing_image, bed, bath, url, has_laundry, has_parking, has_gym, has_hvac, has_wifi, has_game_room, is_pet_friendly, is_accessible)
 VALUES
-(1, '302', '1 Bedroom Apartment', 'Cozy apartment close to university.', 1200.00, 'listing1.jpg',1, 1),
-(1, '102', '2 Bedroom House', 'Spacious house with backyard.', 2200.00,'listing1.jpg', 2, 2),
-(2, '102', 'Studio Near Campus', 'Spacious Studio', 800.00, 'listing1.jpg', 0, 1);
+(1, '302', '1 Bedroom Apartment', 'Cozy apartment close to university.', 1200.00, 'listing1.jpg',1, 1,'https://rentals.ca/waterloo/the-marq-waterloo-at-1-columbia-st-w',1,1,1,1,1,1,1,1),
+(1, '102', '2 Bedroom Apartment', 'Spacious apartment close to Wilfrid Laurier University.', 2200.00,'listing2.jpg', 2, 2,'https://rentals.ca/waterloo/vogue-residences',1,1,1,1,1,1,1,1),
+(2, '102', 'Studio Near Campus', 'Spacious Studio', 800.00, 'listing3.jpg', 0, 1,'https://www.apartments-waterloo-228albert.com/?rcstdid=MzI%3d-GJOvflXLv8M%3d&_yTrackUser=MzM1MTE2OTA2MiMxMzM2NzAxMDE0-FtXlJwAd8xM%3d',1,1,1,1,1,1,1,1);
 
 
 INSERT INTO property (listing_id, street_name, street_number, city, province, postal_code, latitude, longitude)
 VALUES
 (1, 'Columbia Street West', '1', 'Waterloo','ON','N2L 0C8', 43.48164, -80.52640),
 (2, 'King Street North', '155', 'Waterloo','ON','N2J 0EB', 43.47150, -80.52431),
-(3, 'King Street North', '158', 'Waterloo', 'ON', 'N2J 0E5', 43.47183, -80.52375),
-(4, 'Regina Street North', '328', 'Waterloo', 'ON', 'N2J 3A5', 43.48001, -80.52419),
-(5, 'Phillip Street', '252', 'Waterloo', 'ON', 'N2L 0E1', 43.47374, -80.53566),
-(6, 'Phillip Street', '254', 'Waterloo', 'ON', 'N2L 0E1', 43.47391, -80.53647),
-(7, 'Phillip Street,' , '256','Waterloo', 'ON', 'N2L 6B6', 43.47374, -80.53703),
-(8, 'Larch Street', '275', 'Waterloo', 'ON','N2L 0J5', 43.47637, -80.53103),
-(9, 'King Street North', '333', 'Waterloo', 'ON', 'N2J 2Z1', 43.47985, -80.52608),
-(10, 'King Street North', '308', 'Waterloo', 'ON', 'N2J 0G4', 43.47884, -80.52518),
-(11, 'Lester Street', '203', 'Waterloo', 'ON', 'N2L 0B5', 43.47239, -80.53362),
-(12, 'Hemlock Street', '251', 'Waterloo', 'ON', 'N2L 0H2', 43.47514, -80.53162),
-(13, 'Columbia Street West', '145', 'Waterloo', 'ON', 'N2L 0K7', 43.47730, -80.53874),
-(14, 'Lester Street', '181', 'Waterloo', 'ON', 'N2L 0C2', 43.47148, -80.53251),
-(15, 'Phillip Street', '250', 'Waterloo', 'ON', 'N2L 3E9', 43.47332, -80.53660);
+(3, 'King Street North', '158', 'Waterloo', 'ON', 'N2J 0E5', 43.47183, -80.52375);
 
-INSERT INTO property_amenities (property_id, amenities_id)
-VALUES
-(1, 1),
-(1, 2),
-(2, 3),
-(2, 4);
 
 INSERT INTO review (users_id, listing_id, rating, description)
 VALUES
