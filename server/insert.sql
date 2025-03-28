1333c72fbe59051f1d8033ea24e80f82d4d6f6b3
Paarth Bagga <paarthbagga@icloud.com>
2025-03-28 18:04:10 -0400

    Update insert.sql to work with updated schema
    Dummy data

--- server/insert.sql
+++ server/insert.sql
@@ -7,39 +7,19 @@ VALUES
 ('user2', 'Arya', 'Singh', 'aryaspassword', 'arya@gmail.com', 'user'),
 ('user3', 'Aaron', 'Kim', 'aaronspass', 'aaron@wlunest.com', 'admin');
 
-INSERT INTO listings (users_id, unit_number, title, description, price, listing_image, bed, bath)
+INSERT INTO listings (users_id, unit_number, title, description, price, listing_image, bed, bath, url, has_laundry, has_parking, has_gym, has_hvac, has_wifi, has_game_room, is_pet_friendly, is_accessible)
 VALUES
-(1, '302', '1 Bedroom Apartment', 'Cozy apartment close to university.', 1200.00, 'listing1.jpg',1, 1),
-(1, '102', '2 Bedroom House', 'Spacious house with backyard.', 2200.00,'listing1.jpg', 2, 2),
-(2, '102', 'Studio Near Campus', 'Spacious Studio', 800.00, 'listing1.jpg', 0, 1);
+(1, '302', '1 Bedroom Apartment', 'Cozy apartment close to university.', 1200.00, 'listing1.jpg',1, 1,'https://rentals.ca/waterloo/the-marq-waterloo-at-1-columbia-st-w',1,1,1,1,1,1,1,1),
+(1, '102', '2 Bedroom Apartment', 'Spacious apartment close to Wilfrid Laurier University.', 2200.00,'listing2.jpg', 2, 2,'https://rentals.ca/waterloo/vogue-residences',1,1,1,1,1,1,1,1),
+(2, '102', 'Studio Near Campus', 'Spacious Studio', 800.00, 'listing3.jpg', 0, 1,'https://www.apartments-waterloo-228albert.com/?rcstdid=MzI%3d-GJOvflXLv8M%3d&_yTrackUser=MzM1MTE2OTA2MiMxMzM2NzAxMDE0-FtXlJwAd8xM%3d',1,1,1,1,1,1,1,1);
 
-INSERT INTO property (listing_id, street_name, street_number, city, province, postal_code)
-VALUES
-(1, 'King Street North', '167', 'Waterloo','ON','N2J 0B6'),
-(2, 'Albert Street', '202', 'Waterloo','ON','N2L 0B6');
 
-INSERT INTO floor_plan (property_id, floor_plan_name, floor_plan_image)
-VALUES
-(1, 'Standard One Bedroom', 'floor_plan1.jpg'),
-(2, 'Deluxe Suite', 'floorplan2.jpg');
-
-INSERT INTO amenities (property_id, amenities_name)
-VALUES
-(1, 'Gym'),
-(1, 'Laundry Room'),
-(2, 'Pool'),
-(2, 'Parking Garage');
 INSERT INTO property (listing_id, street_name, street_number, city, province, postal_code, latitude, longitude)
 VALUES
-(1, 'King Street North', '167', 'Waterloo','ON','N2J 0B6', 43.475503, -80.528091),
-(2, 'Albert Street', '202', 'Waterloo','ON','N2L 0B6', 43.474610, -80.531839);
+(1, 'Columbia Street West', '1', 'Waterloo','ON','N2L 0C8', 43.48164, -80.52640),
+(2, 'King Street North', '155', 'Waterloo','ON','N2J 0EB', 43.47150, -80.52431),
+(3, 'King Street North', '158', 'Waterloo', 'ON', 'N2J 0E5', 43.47183, -80.52375);
 
-INSERT INTO property_amenities (property_id, amenities_id)
-VALUES
-(1, 1),
-(1, 2),
-(2, 3),
-(2, 4);
 
 INSERT INTO review (users_id, listing_id, rating, description)
 VALUES
